5c2cf0448820688fab4077febcb2c9e5270c58c1
mislav dobrinic <mislav.dobrinic@b2match.com>
2024-08-26 15:12:59 +0200

    setup good, webpack package needs to be checked to make it work

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,29 @@
 import NextFederationPlugin from "@module-federation/nextjs-mf";
 
-const getFederationConfig = (isServer) => ({
-  name: "admin-next",
-  remotes: {},
-  filename: "static/chunks/remoteEntry.js",
-  // exposes: {
-  //   "./dashboard": "./pages/dashboard",
-  // },
-  shared: {
-    next: {
-      requiredVersion: "14.2.4",
-      singleton: true,
-    },
-  },
-  extraOptions: {
-    debug: false, // `false` by default
-    exposePages: true, // `false` by default
-    enableImageLoaderFix: false, // `false` by default
-    enableUrlLoaderFix: false, // `false` by default
-    skipSharingNextInternals: false, // `false` by default
-  },
-});
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ["shared"],
   webpack(config, options) {
-    const { isServer } = options;
-
-    const mfConf = getFederationConfig(isServer);
-
-    config.plugins.push(new NextFederationPlugin(mfConf));
-
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "admin-next",
+          remotes: {},
+          filename: "static/chunks/remoteEntry.js",
+          exposes: {
+            "./dashboard": "./pages/dashboard.tsx",
+          },
+          shared: {
+            react: {
+              requiredVersion: false,
+              singleton: true,
+            },
+          },
+        })
+      );
+    }
     return config;
   },
+  // your original next.config.js export
+  reactStrictMode: true,
 };
 
 export default nextConfig;
